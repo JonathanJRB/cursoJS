8b8bf0703f26152be097b63a1db51c5b3a1e11c5
Jonathan <jonathan-jrb@outlook.com>
2025-05-31 23:08:46 +0100

    310525

---
+++ aula-03.js
@@ -0,0 +1,11 @@
+const idade = 20;
+let podeEntrar = false;
+
+if (idade >= 18) {
+    podeEntrar = true;
+    }
+else {
+    console.log('NAO PODE ENTRAR');
+}
+console.log('PODE ENTRAR', podeEntrar);
+
